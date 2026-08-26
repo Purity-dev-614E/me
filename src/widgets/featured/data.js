f85529ae2c0b2c5da209143d779574711cb42bfb
Purity <puritysang180@gmail.com>
2026-08-26 19:12:23 +0300

    prod 2

--- src/widgets/featured/data.js
+++ src/widgets/featured/data.js
@@ -17,5 +17,5 @@ export const featuredProjectData = {
     { src: "/screenshots/screenshot3.jpeg", alt: "Safari  group mngmt" },
     { src: "/screenshots/screenshot4.jpeg", alt: "Safari  sign up" },
   ],
-  link: { label: "https://churchconnect-ashen.vercel.app/", href: "https://churchconnect-ashen.vercel.app/" },
+  link: { label: "safariconnect.org", href: "https://churchconnect-ashen.vercel.app/" },
 };
